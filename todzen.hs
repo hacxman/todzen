11395400a2268598c8a646501e28fb5ef9e2c294
Maros Zatko <hacxman@gmail.com>
2014-01-04 16:55:50 +0100

    mods for home ws

--- todzen.hs
+++ todzen.hs
@@ -25,31 +25,33 @@ updateStatus hnd i = do
   (loadavg, cpus) <- loadAvg
   let load_red = floor $ min 255 $ 155.0 + loadavg * (100.0 / fromIntegral cpus)
 
-  (bat, ac, batcap) <- batInfo
-  let ac_bat_string' = show bat ++ " " ++ show ac
-  let ac_bat_string'' | ac == Online = "AC " ++
-                        case bat of Unknown  -> "C"
-                                    Charging -> "C"
-                                    Full     -> "F"
-                                    _        -> "Err"
-                    | ac == Offline =
-                        case bat of Discharging -> "D"
-                                    Full        -> "F"
-                                    Empty       -> "E"
-                                    _        -> "Err"
 
-  let ac_bat_string | ac == Online = "AC " ++ (head $ show bat) : []
-                    | ac == Offline = (head $ show bat) : []
+--  (bat, ac, batcap) <- batInfo
+--  let ac_bat_string' = show bat ++ " " ++ show ac
+--  let ac_bat_string'' | ac == Online = "AC " ++
+--                        case bat of Unknown  -> "C"
+--                                    Charging -> "C"
+--                                    Full     -> "F"
+--                                    _        -> "Err"
+--                    | ac == Offline =
+--                        case bat of Discharging -> "D"
+--                                    Full        -> "F"
+--                                    Empty       -> "E"
+--                                    _        -> "Err"
+--
+--  let ac_bat_string | ac == Online = "AC " ++ (head $ show bat) : []
+--                    | ac == Offline = (head $ show bat) : []
 
   vol <- volume
+  print vol
   hPutStrLn hnd $ Prelude.concatMap (wrap " " " ") $
 --  putStrLn $ Prelude.concatMap (wrap " " " ") $
     [dzenColor "#cccc22" "" (clicable (dzenEscape " - ") "5" "0x1008FF11")
     ++"/"++dzenColor "#cccc22" "" (clicable (dzenEscape " + ") "4" "0x1008FF13")
     ++dzenColor "#cccc22" "" (hBar 50 10 100 (floor $ 100*vol))
     ,dzenColor "#cccc22" "" $ show $ floor (vol*100)
-    ,dzenColor "#cc7722" "" $ hBar 50 10 100 (floor $ 100*batcap)
-    ,dzenColor "#cc7722" "" $ ac_bat_string ++ " " ++ show (floor $ 100*batcap) ++ "%"
+--    ,dzenColor "#cc7722" "" $ hBar 50 10 100 (floor $ 100*batcap)
+  --  ,dzenColor "#cc7722" "" $ ac_bat_string ++ " " ++ show (floor $ 100*batcap) ++ "%"
     ,dzenColor ("#"++showHex load_red "9999") "" $ printf "%.2fL" loadavg
     ,dzenColor "#77bb22" "" $ hBar 50 10 memT (memT - memF)
     ,dzenColor "#77bb22" ""
@@ -65,21 +67,22 @@ clicable l b key = "^ca("++b++",xdotool key "++key++")"++l++"^ca()"
 
 hBar w h max val =
       "^ib(1)^ro("++show w++"x10)^p(-"++show (w-2)++")^r("
-      ++ show (pixval-4) ++"x6)"
-      ++ "^p(+" ++ show (w-pixval+2) ++ ")^ib(0)"
+      ++ show pixval ++"x6)"
+      ++ "^p(+" ++ show (w-pixval-2) ++ ")^ib(0)"
   where
-    pixval = floor (fromIntegral w * (fromIntegral val / fromIntegral max))
+    pixval = floor (fromIntegral (w - 4) * (fromIntegral val / fromIntegral max))
 
-dzenCmd = "dzen2 -x '600' -w '800' -ta 'r'" ++ style
+dzenCmd = "dzen2 -x '600' -w '1250' -ta 'r'" ++ style
 style   = " -h '16' -fg '#777777' -bg '#222222' -fn 'arial:bold:size=9'"
 
 volume :: IO Float
 volume = do
-  con <- readProcess "amixer" ["-c", "0", "sget", "Master"] []
+  con <- readProcess "amixer" ["sget", "Master"] []
+  --print $ (con =~ "^.*Playback.*\\[([0-9]*)%\\].*$" :: [[String]])
   return $ extractVol con
   where
     extractVol con = ((/100).read) (vol++".0")
-      where [[_,vol]] = con =~ "^.*Playback.*\\[([0-9]*)%\\].*$" :: [[String]]
+      where [[_,vol],_] = con =~ "^.*Playback.*\\[([0-9]*)%\\].*$" :: [[String]]
 
 loadAvg :: IO (Float, Int)
 loadAvg = do
